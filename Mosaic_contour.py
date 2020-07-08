2536122400d459ca06f01da743b49a15b869c229
mrumayee <66308881+mrumayee@users.noreply.github.com>
2020-07-08 17:58:29 +0530

    Update Mosaic_contour.py

--- Mosaic_contour.py
+++ Mosaic_contour.py
@@ -5,11 +5,6 @@ import random as rd
 image = cv2.imread ("mos.jpg")
 img_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 ret,global_thresh = cv2.threshold(img_gray,1,255,cv2.THRESH_BINARY)
-
-
-
-
-
 orig = np.copy (image)
 width = global_thresh.shape [1]
 height = global_thresh.shape [0]
@@ -55,14 +50,14 @@ for t in triangleList:
 contours,hierarchy=cv2.findContours(global_thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 for contour in contours:
     approx=cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour,True),True)
-    cv2.drawContours(image,[approx],0,(0,0,0),2)
-    x=approx.ravel()[0]
-    y=approx.ravel()[1]
-    if len(approx)==3:
-        cv2.putText(image,'triangle',(x,y),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,0))
+    cv2.drawContours(image,[approx],0,(0,255,0),2)
+    #x=approx.ravel()[0]
+    #y=approx.ravel()[1]
+    #if len(approx)==3:
+        #cv2.putText(image,'',(x,y),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,0))
 
 
 #cv2.namedWindow ('orig', cv2.WINDOW_NORMAL)
-cv2.imshow ('orig', image)
+cv2.imshow ('orignal', image)
 cv2.imshow("Global_Thresh",global_thresh)
 cv2.waitKey (0)
